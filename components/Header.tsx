c3e4d42cbdbe7473fe526edd122cff6dca2c8f52
garydsong <ceouls@gmail.com>
2023-08-01 15:02:50 -0700

    sign up and login buttons

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,6 +5,7 @@ import { twMerge } from "tailwind-merge";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx"
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
+import Button from "./Button";
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -43,8 +44,18 @@ const Header: React.FC<HeaderProps> = ({
             <BiSearch className="text-black" size={20} />
           </button>
         </div>
-        <div className="flex justify-between items-center gap-x-4"></div>
+        <div className="flex justify-between items-center gap-x-4">
+          <>
+          <div>
+            <Button onClick={() => {}} className="bg-transparent text-neutral-300 font-medium">Sign Up</Button>
+          </div>
+          <div>
+            <Button onClick={() => {}} className="bg-white px-6 py-2">Log in</Button>
+          </div>
+          </>
+        </div>
       </div>
+      {children}
     </div>
   )
 }
