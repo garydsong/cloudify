ab08688505c79701e320c60d88400fcf0ecb7585
garydsong <ceouls@gmail.com>
2023-08-02 08:21:56 -0700

    supabase provider

--- providers/SupabaseProvider.tsx
+++ providers/SupabaseProvider.tsx
@@ -1,6 +1,8 @@
 "use client"
 import { Database } from "@/types_db"
-import React from "react"
+import React, { useState } from "react"
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
+import { SessionContextProvider } from "@supabase/auth-helpers-react"
 interface SupabaseProviderProps {
     children: React.ReactNode;
 }
@@ -8,7 +10,14 @@ interface SupabaseProviderProps {
 const SupabaseProvider: React.FC<SupabaseProviderProps> = ({
     children
 }) => {
-    return ()
+    const [supabaseClient] = useState(() =>
+        createClientComponentClient<Database>()
+    )
+    return (
+        <SessionContextProvider supabaseClient={supabaseClient}>
+            {children}
+        </SessionContextProvider>
+    )
 }
 
 export default SupabaseProvider
