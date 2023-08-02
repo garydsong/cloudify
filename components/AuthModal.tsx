149ae1922641221b300e326e9482dddf8ae4f76e
garydsong <ceouls@gmail.com>
2023-08-02 12:06:55 -0700

    login and logout working -- log in with github working

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -5,12 +5,20 @@ import Modal from "./Modal"
 import {Auth} from "@supabase/auth-ui-react"
 import { ThemeSupa } from "@supabase/auth-ui-shared"
 import useAuthModal from "@/hooks/useAuthModal"
+import { useEffect } from "react"
 
 const AuthModal = () => {
     const supabaseClient = useSupabaseClient();
     const router = useRouter();
     const { session } = useSessionContext();
     const { onClose, isOpen } = useAuthModal();
+
+    useEffect(() => {
+        if (session) {
+            router.refresh();
+            onClose();
+        }
+    }, [session, router, onClose])
 
     const onChange = (open: boolean) => {
         if (!open) onClose();
@@ -25,7 +33,7 @@ const AuthModal = () => {
         >
             <Auth
                 theme="dark"
-                providers={["facebook", "google", "twitch"]}
+                providers={["facebook", "google", "github"]}
                 supabaseClient={supabaseClient}
                 appearance={{
                     theme: ThemeSupa,
--- components/Header.tsx
+++ components/Header.tsx
@@ -6,6 +6,10 @@ import { RxCaretLeft, RxCaretRight } from "react-icons/rx"
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
 import Button from "./Button";
+import useAuthModal from "@/hooks/useAuthModal";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useUser } from "@/hooks/useUser";
+import { FaUserAlt } from "react-icons/fa";
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -16,17 +20,26 @@ const Header: React.FC<HeaderProps> = ({
   children,
   className
 }) => {
+  const authModal = useAuthModal();
   const router = useRouter();
-  const handleLogout = () => {
-    // handle logout
+
+  const supabaseClient = useSupabaseClient();
+  const { user } = useUser();
+
+  const handleLogout = async () => {
+    const { error } = await supabaseClient.auth.signOut();
+    router.refresh()
+
+    if (error) console.log(error)
   }
+
   return (
     <div className={twMerge(`
     h-fit
     bg-gradient-to-b
     from-emerald-800
     p-6`,
-    className)}>
+      className)}>
       <div className="w-full mb-4 flex items-center justify-between">
         <div className="hidden md:flex gap-x-2 items-center">
           <button onClick={() => router.back()} className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition">
@@ -45,14 +58,25 @@ const Header: React.FC<HeaderProps> = ({
           </button>
         </div>
         <div className="flex justify-between items-center gap-x-4">
+          {user ? (
+            <div className="flex gap-x-4 items-center">
+              <Button onClick={handleLogout} className="bg-white px-6 py-2">
+                Logout
+              </Button>
+              <Button onClick={() => router.push("/account")} className="bg-white">
+                <FaUserAlt />
+              </Button>
+            </div>
+          ) : (
           <>
-          <div>
-            <Button onClick={() => {}} className="bg-transparent text-neutral-300 font-medium">Sign Up</Button>
-          </div>
-          <div>
-            <Button onClick={() => {}} className="bg-white px-6 py-2">Log in</Button>
-          </div>
+            <div>
+              <Button onClick={authModal.onOpen} className="bg-transparent text-neutral-300 font-medium">Sign Up</Button>
+            </div>
+            <div>
+              <Button onClick={authModal.onOpen} className="bg-white px-6 py-2">Log in</Button>
+            </div>
           </>
+          )}
         </div>
       </div>
       {children}
