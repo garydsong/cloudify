e2991d15e9c452923b285cecc5b322c505b9625d
garydsong <ceouls@gmail.com>
2023-08-03 09:10:42 -0700

    next config with supabase bucket showing album art

--- components/SongItem.tsx
+++ components/SongItem.tsx
@@ -1,7 +1,9 @@
 "use client"
 
+import useLoadImage from "@/hooks/useLoadImage"
 import { Song } from "@/types"
 import React from "react"
+import Image from "next/image"
 
 interface SongItemProps {
     data: Song
@@ -12,9 +14,47 @@ const SongItem: React.FC<SongItemProps> = ({
     data,
     onClick
 }) => {
-  return (
-    <div>SongItem</div>
-  )
+    const imagePath = useLoadImage(data);
+
+    return (
+        <div
+            onClick={() => onClick(data.id)}
+            className="
+                relative
+                group
+                flex
+                flex-col
+                items-center
+                justify-center
+                rounded-md
+                overflow-hidden
+                gap-x-4
+                bg-neutral-400/5
+                cursor-pointer
+                hover:bg-neutral-400/10
+                transition
+                p-3
+            "
+        >
+            <div
+                className="
+                    relative
+                    aspect-square
+                    w-full
+                    h-full
+                    rounded-md
+                    overflow-hidden
+                "
+            >
+                <Image
+                    className="object-cover"
+                    src={imagePath || '/images/liked.png'}
+                    fill
+                    alt="Image"
+                />
+            </div>
+        </div>
+    )
 }
 
 export default SongItem
