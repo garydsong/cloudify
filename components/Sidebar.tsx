abe6304028f5a2729720534e6ba0d8c9e5408bb1
garydsong <ceouls@gmail.com>
2023-08-03 12:51:41 -0700

    media component and library populating

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,13 +7,16 @@ import { usePathname } from "next/navigation"
 import { useMemo } from "react";
 import { HiHome } from "react-icons/hi"
 import { BiSearch } from "react-icons/bi"
+import { Song } from "@/types";
 
 interface SidebarProps {
     children: React.ReactNode;
+    songs: Song[];
 }
 
 const Sidebar: React.FC<SidebarProps> = ({
-    children
+    children,
+    songs
 }) => {
     const pathname = usePathname()
     const routes = useMemo(() => [
@@ -42,7 +45,7 @@ const Sidebar: React.FC<SidebarProps> = ({
                     </div>
                 </Box>
                 <Box className="overflow-y-auto h-full">
-                    <Library></Library>
+                    <Library songs={songs}></Library>
                 </Box>
             </div>
             <main className="h-full flex-1 overflow-y-auto py-2">
