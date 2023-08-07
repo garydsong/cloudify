f3dae1cf07142dde4e8ed5899563024b51ffedfb
garydsong <ceouls@gmail.com>
2023-08-07 13:28:55 -0700

    complete

--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -3,6 +3,7 @@ import useLoadImage from "@/hooks/useLoadImage";
 import { Song } from "@/types";
 import React from "react";
 import Image from "next/image";
+import usePlayer from "@/hooks/usePlayer";
 
 
 interface MediaItemProps {
@@ -14,29 +15,32 @@ const MediaItem: React.FC<MediaItemProps> = ({
     data,
     onClick
 }) => {
+    const player = usePlayer();
     const imageUrl = useLoadImage(data);
     const handleClick = () => {
         if (onClick) {
             return onClick(data.id)
         }
+
+        return player.setId(data.id)
     }
 
-  return (
-    <div onClick={handleClick} className="flex items-center gap-x-3 cursor-pointer hover:bg-neutral-800/50 w-full p-2 rounded-md">
-        <div className="relative rounded-md min-h-[48px] min-w-[48px] overflow-hidden">
-            <Image
-                fill
-                src={imageUrl || '/images/liked.png'}
-                alt="Media Item"
-                className="object-cover"
-            />
+    return (
+        <div onClick={handleClick} className="flex items-center gap-x-3 cursor-pointer hover:bg-neutral-800/50 w-full p-2 rounded-md">
+            <div className="relative rounded-md min-h-[48px] min-w-[48px] overflow-hidden">
+                <Image
+                    fill
+                    src={imageUrl || '/images/liked.png'}
+                    alt="Media Item"
+                    className="object-cover"
+                />
+            </div>
+            <div className="flex flex-col gap-y-1 overflow-hidden">
+                <p className="text-white truncate">{data.title}</p>
+                <p className="text-neutral-400 text-sm truncate">{data.author}</p>
+            </div>
         </div>
-        <div className="flex flex-col gap-y-1 overflow-hidden">
-            <p className="text-white truncate">{data.title}</p>
-            <p className="text-neutral-400 text-sm truncate">{data.author}</p>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default MediaItem
